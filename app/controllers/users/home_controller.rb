555adf0f8a5278e9b40c5df6911ea655924cb10e
Gautam <khushbu.goyal@berylsystems.com>
2016-10-12 20:54:23 +0530

    Devise addition for user

---
+++ app/controllers/users/home_controller.rb
@@ -0,0 +1,23 @@
+class Users::HomeController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
